7f9d69e4480d854770d53cc4849603ce92307f98
YuH25JP <cppyuh@gmail.com>
2023-10-28 15:22:02 +0900

    adds a progress bar

--- src/components/Statistics.vue
+++ src/components/Statistics.vue
@@ -1,6 +1,13 @@
 <template>
   <v-row justify="center" class="ma-2">
-    {{ timeHistoryStore.hist.length }}/5
+    <div class="container" style="width: 550px">
+      <v-progress-linear
+        v-model="solvePercent"
+        color="primary"
+        height="20"
+        rounded
+      ><strong>{{ historyLength }}/5</strong></v-progress-linear>
+    </div>
   </v-row>
   <v-row justify="center" class="font-weight-bold ma-2">
     Ao5: --.---
@@ -8,11 +15,12 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
 import { useTimeHistoryStore } from '@/store/timeHistory';
 const timeHistoryStore = useTimeHistoryStore();
-let historyLength = ref(timeHistoryStore.hist.length);
+let historyLength = computed(() => timeHistoryStore.hist.length);
+const solvePercent = ref(computed(() => timeHistoryStore.hist.length * 20));
 console.log(`History Length: ${historyLength.value}`);
 
 function calculateAo5(): number | null {
